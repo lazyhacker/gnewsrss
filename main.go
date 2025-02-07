0efc32711a4810b6755e831f69fd19e24946ed73
LazyHacker <github@lazyhacker.com>
2025-02-06 21:25:10 -0800

    Clean up print lines (changed to log where kept) Added new feed.

--- main.go
+++ main.go
@@ -39,19 +39,16 @@ func main() {
 		"https://news.google.com/rss",
 		"https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", // World
 		"https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", // Business
+		"https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", // Technology
+		"https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", // Entertainment
+		"https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", // Sports
 	}
 
 	var headlines, filteredHeadlines []Item
-	/*
-		topics := []string{
-			"CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB", // World
-			"CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB", // Business
-		}
-	*/
 
 	f, err := os.Create(filepath.Join("docs", "headlines.json"))
 	if err != nil {
-		fmt.Println("Error creating or opening the file:", err)
+		log.Print("Error creating or opening the file:", err)
 		return
 	}
 	defer f.Close()
@@ -74,31 +71,9 @@ func main() {
 	model.SystemInstruction = &genai.Content{
 		Parts: []genai.Part{genai.Text("You are a news filter who will be given a list of news headlines prepended with an index value to filter out any that are political in nature.  Any headline that mentions Trump are considered political and should be removed.  Also filter out any headlines that include individuals such as Elon Musk who are political figures even though they aren't politicians. Also filter out individuals who are known wealthy politicial donors. Only return the index value of the headlines that are non-political as a comma separated list.")},
 	}
-	/*
-		model.ResponseSchema = &genai.Schema{
-			Type: genai.TypeObject,
-			Properties: map[string]*genai.Schema{
-				"headlines": &genai.Schema{
-					Type: genai.TypeArray,
-					Items: &genai.Schema{
-						Type: genai.TypeObject,
-						Properties: map[string]*genai.Schema{
-							"title": &genai.Schema{
-								Type: genai.TypeString,
-							},
-							"link": &genai.Schema{
-								Type: genai.TypeString,
-							},
-						},
-					},
-				},
-			},
-		}
-	*/
 
 	for _, u := range feedURLs {
 		// Fetch RSS feed
-		//u := fmt.Sprintf("https://news.google.com/rss/topics/%v/?hl=en-US&gl=US&ceid=US%3Aen", t)
 		resp, err := http.Get(u)
 		if err != nil {
 			log.Fatalf("Error fetching RSS feed: %v", err)
@@ -114,23 +89,10 @@ func main() {
 		if err := xml.Unmarshal(body, &rss); err != nil {
 			log.Fatalf("Error parsing RSS feed: %v", err)
 		}
-		filename := fmt.Sprintf("%v.json", strings.ReplaceAll(rss.Channel.Title, " ", ""))
-		f, err := os.Create(filepath.Join("docs", filename))
-		if err != nil {
-			fmt.Println("Error creating or opening the file:", err)
-			return
-		}
-		defer f.Close()
-
-		// Print feed title
-		//fmt.Println("Feed Title:", rss.Channel.Title)
-
-		//prompt.WriteString(fmt.Sprintf("Feed Title: %v\n", rss.Channel.Title))
 
 		// Print the titles and links of the latest articles
 		for _, item := range rss.Channel.Items {
-			fmt.Printf("- %s (%s)\n", item.Title, item.Link)
-			//fmt.Printf("%s\n", item.Title)
+			//fmt.Printf("- %s (%s)\n", item.Title, item.Link)
 			headlines = append(headlines, item)
 		}
 	}
@@ -139,9 +101,8 @@ func main() {
 		prompt.WriteString(fmt.Sprintf("%d %v\n", idx, item.Title))
 	}
 
-	fmt.Println("------------Calling Gemini--------------")
-	fmt.Printf("Prompt:\n%s", prompt.String())
-	fmt.Println("---------------------")
+	log.Println("------------Calling Gemini--------------")
+	//fmt.Printf("Prompt:\n%s", prompt.String())
 	chat := model.StartChat()
 	res, err := chat.SendMessage(ctx, genai.Text(prompt.String()))
 	if err != nil {
@@ -151,12 +112,12 @@ func main() {
 	for _, part := range res.Candidates[0].Content.Parts {
 		//fmt.Printf("[%d]:\n%v\n", i, part)
 		partStr := fmt.Sprintf("%v", part)
-		fmt.Printf("partStr = %v\n", partStr)
+		//fmt.Printf("partStr = %v\n", partStr)
 		ss := strings.Split(partStr, ",")
 		for j := 0; j < len(ss); j++ {
 			x, err := strconv.Atoi(strings.TrimSpace(ss[j]))
 			if err != nil {
-				fmt.Printf("Unable to convert to int: %v", err)
+				log.Printf("Unable to convert to int: %v", err)
 				continue
 			}
 			filteredHeadlines = append(filteredHeadlines, headlines[x])
@@ -167,7 +128,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Writing to headlines.json")
+	log.Print("Writing to headlines.json")
 	if _, err := f.WriteString(string(jsonData)); err != nil {
 		panic(err)
 	}
